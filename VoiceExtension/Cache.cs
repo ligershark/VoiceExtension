94ab25cae9ae0d55e9f05fc59c079220daea7140
Mads Kristensen <post@madskristensen.net>
2013-05-17 08:03:10 -0700

    Updated cache

---
+++ VoiceExtension/Cache.cs
@@ -0,0 +1,35 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using System.Collections.Generic;
+
+namespace MadsKristensen.VoiceExtension
+{
+    internal class Cache
+    {
+        public static Dictionary<string, string> Commands { get; set; }
+
+        public static void BuildCommandList(DTE2 dte)
+        {
+            Commands = new Dictionary<string, string>();
+
+            foreach (Command cmd in dte.Commands)
+            {
+                string name = cmd.Name.Substring(cmd.Name.LastIndexOf('.') + 1);
+                string final = string.Empty;
+
+                for (int i = name.Length - 1; i > -1; i--)
+                {
+                    final = final.Insert(0, name[i].ToString());
+
+                    if (i > 0 && char.IsUpper(name[i]))
+                    {
+                        final = final.Insert(0, " ");
+                    }
+                }
+
+                if (!string.IsNullOrEmpty(final) && !Commands.ContainsKey(final))
+                    Commands.Add(final, cmd.Name);
+            }
+        }
+    }
+}
--- VoiceExtension/VoiceExtensionPackage.cs
+++ VoiceExtension/VoiceExtensionPackage.cs
@@ -3,9 +3,7 @@ using EnvDTE80;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.Design;
-using System.Globalization;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Speech.Recognition;
@@ -16,18 +14,19 @@ namespace MadsKristensen.VoiceExtension
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [Guid(GuidList.guidVoiceExtensionPkgString)]
+    [ProvideAutoLoad(UIContextGuids80.EmptySolution)]
     [ProvideAutoLoad(UIContextGuids80.SolutionExists)]
     public sealed class VoiceExtensionPackage : Package
     {
+        private const float _confidence = 0.87F;
+
         private DTE2 _dte;
         private SpeechRecognitionEngine _rec;
-        private Dictionary<string, string> _dic;
 
         protected override void Initialize()
         {
             base.Initialize();
             _dte = GetService(typeof(DTE)) as DTE2;
-            _dic = new Dictionary<string, string>();
 
             OleMenuCommandService mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
             if (null != mcs)
@@ -37,7 +36,7 @@ namespace MadsKristensen.VoiceExtension
                 mcs.AddCommand(menu);
             }
 
-            BuildCommandList();
+            Cache.BuildCommandList(_dte);
             InitializeSpeechRecognition();
         }
 
@@ -50,11 +49,11 @@ namespace MadsKristensen.VoiceExtension
 
         private void InitializeSpeechRecognition()
         {
-            var c = new Choices(_dic.Keys.ToArray());
+            var c = new Choices(Cache.Commands.Keys.ToArray());
             var gb = new GrammarBuilder(c);
             var g = new Grammar(gb);
 
-            _rec = new SpeechRecognitionEngine(new CultureInfo("en"));
+            _rec = new SpeechRecognitionEngine();
             _rec.RecognizeCompleted += OnRecognizeCompleted;
             _rec.InitialSilenceTimeout = TimeSpan.FromSeconds(3);
 
@@ -66,9 +65,9 @@ namespace MadsKristensen.VoiceExtension
         {
             _rec.RecognizeAsyncStop();
 
-            if (e.Result != null && e.Result.Confidence > 0.87 && _dic.ContainsKey(e.Result.Text))
+            if (e.Result != null && e.Result.Confidence > _confidence && Cache.Commands.ContainsKey(e.Result.Text))
             {
-                if (TryExecuteCommand(_dic[e.Result.Text]))
+                if (TryExecuteCommand(Cache.Commands[e.Result.Text]))
                     _dte.StatusBar.Text = e.Result.Text;
                 else
                     _dte.StatusBar.Text = e.Result.Text + " is not available in this context";
@@ -80,28 +79,6 @@ namespace MadsKristensen.VoiceExtension
             else
             {
                 _dte.StatusBar.Clear();
-            }
-        }
-
-        private void BuildCommandList()
-        {
-            foreach (Command cmd in _dte.Commands)
-            {
-                string name = cmd.Name.Substring(cmd.Name.LastIndexOf('.') + 1);
-                string final = string.Empty;
-
-                for (int i = name.Length - 1; i > -1; i--)
-                {
-                    final = final.Insert(0, name[i].ToString());
-
-                    if (i > 0 && char.IsUpper(name[i]))
-                    {
-                        final = final.Insert(0, " ");
-                    }
-                }
-
-                if (!string.IsNullOrEmpty(final) && !_dic.ContainsKey(final))
-                    _dic.Add(final, cmd.Name);
             }
         }
 
