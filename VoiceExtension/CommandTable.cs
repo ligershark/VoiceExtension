fe6764fd1cb8d2e2babcd0ef7c310c1b9be7c8f8
Mads Kristensen <post@madskristensen.net>
2013-05-18 11:59:53 -0700

    Refactorings and tweaks

---
+++ VoiceExtension/CommandTable.cs
@@ -0,0 +1,120 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace MadsKristensen.VoiceExtension
+{
+    internal class CommandTable
+    {
+        private DTE2 _dte;
+
+        public CommandTable(DTE2 dte)
+        {
+            _dte = dte;
+
+            BuildCommandTable();
+        }
+
+        public Dictionary<string, Command> Commands { get; set; }
+
+        private void BuildCommandTable()
+        {
+            Commands = new Dictionary<string, Command>();
+
+            string folder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string file = Path.Combine(folder, "resources", "commands.txt");
+
+            if (File.Exists(file))
+            {
+                string[] lines = File.ReadAllLines(file);
+
+                foreach (string line in lines)
+                {
+                    if (string.IsNullOrWhiteSpace(line) || line.StartsWith("#"))
+                        continue;
+
+                    string[] args = line.Split('|');
+
+                    if (args.Length == 2)
+                        AddCommand(args[1], args[0]);
+                }
+            }
+        }
+
+        //private void GenerateCommandFile()
+        //{
+        //    foreach (Command cmd in _dte.Commands)
+        //    {
+        //        string name = cmd.Name.Substring(cmd.Name.LastIndexOf('.') + 1);
+        //        string realName = string.Empty;
+
+        //        for (int i = name.Length - 1; i > -1; i--)
+        //        {
+        //            realName = realName.Insert(0, name[i].ToString());
+
+        //            if (i > 0 && char.IsUpper(name[i]))
+        //            {
+        //                realName = realName.Insert(0, " ");
+        //            }
+        //        }
+
+        //        AddCommand(cmd.Name, realName);
+        //    }
+
+        //    using (var fr = new System.IO.StreamWriter("c:\\users\\madsk\\commands.txt"))
+        //    {
+        //        foreach (string key in Commands.Keys)
+        //        {
+        //            fr.WriteLine(key + "|" + Commands[key].Name);
+        //        }
+        //    }
+        //}
+
+        private void AddCommand(string commandName, string realName)
+        {
+            string clean = realName
+                            .Replace("\"", string.Empty)
+                            .Replace("'", string.Empty);
+
+            if (!string.IsNullOrEmpty(clean) && !Commands.ContainsKey(clean))
+            {
+                try
+                {
+                    var command = _dte.Commands.Item(commandName);
+                    Commands.Add(clean, command);
+                }
+                catch { /* The command doesn't exist in the installed version of VS */ }
+            }
+        }
+
+        public void ExecuteCommand(string displayName)
+        {
+            var command = Commands[displayName];
+
+            if (command.IsAvailable)
+            {
+                _dte.ExecuteCommand(command.Name);
+                DisplayKeyBindings(displayName, command);
+            }
+            else
+            {
+                _dte.StatusBar.Text = displayName + " is not available in this context";
+            }
+        }
+
+        private void DisplayKeyBindings(string displayName, Command command)
+        {
+            _dte.StatusBar.Text = displayName;
+            var bindings = ((object[])command.Bindings).FirstOrDefault() as string;
+
+            if (!string.IsNullOrEmpty(bindings))
+            {
+                int index = bindings.IndexOf(':') + 2;
+                _dte.StatusBar.Text += " (" + bindings.Substring(index) + ")";
+            }
+        }
+    }
+}
--- VoiceExtension/VoiceExtensionPackage.cs
+++ VoiceExtension/VoiceExtensionPackage.cs
@@ -17,27 +17,46 @@ namespace MadsKristensen.VoiceExtension
     [Guid(GuidList.guidVoiceExtensionPkgString)]
     public sealed class VoiceExtensionPackage : Package
     {
-        private const float _confidence = 0.8F;
+        private const float _confidence = 0.80F;
 
         private DTE2 _dte;
+        private CommandTable _cache;
         private SpeechRecognitionEngine _rec;
         private bool _isEnabled;
 
         protected override void Initialize()
         {
-            base.Initialize();
             _dte = GetService(typeof(DTE)) as DTE2;
-
-            OleMenuCommandService mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
-            if (null != mcs)
-            {
-                CommandID cmd = new CommandID(GuidList.guidVoiceExtensionCmdSet, (int)PkgCmdIDList.cmdidMyCommand);
-                MenuCommand menu = new MenuCommand(OnListening, cmd);
-                mcs.AddCommand(menu);
-            }
-
-            Cache.BuildCommandList(_dte);
+            _cache = new CommandTable(_dte);
             InitializeSpeechRecognition();
+
+            // Setup listening command
+            OleMenuCommandService mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            CommandID cmd = new CommandID(GuidList.guidVoiceExtensionCmdSet, (int)PkgCmdIDList.cmdidMyCommand);
+            MenuCommand menu = new MenuCommand(OnListening, cmd);
+            mcs.AddCommand(menu);
+        }
+
+        private void InitializeSpeechRecognition()
+        {
+            try
+            {
+                var c = new Choices(_cache.Commands.Keys.ToArray());
+                var gb = new GrammarBuilder(c);
+                var g = new Grammar(gb);
+
+                _rec = new SpeechRecognitionEngine();
+                _rec.InitialSilenceTimeout = TimeSpan.FromSeconds(3);
+                _rec.SpeechHypothesized += OnSpeechHypothesized;
+                _rec.RecognizeCompleted += OnSpeechRecognized;
+
+                _rec.LoadGrammarAsync(g);
+                _rec.SetInputToDefaultAudioDevice();
+
+                _isEnabled = true;
+            }
+            catch
+            { }
         }
 
         private void OnListening(object sender, EventArgs e)
@@ -48,7 +67,6 @@ namespace MadsKristensen.VoiceExtension
                 _rec.RecognizeAsync();
 
                 _dte.StatusBar.Text = "I'm listening...";
-                _dte.StatusBar.Highlight(true);
             }
             else
             {
@@ -56,41 +74,22 @@ namespace MadsKristensen.VoiceExtension
             }
         }
 
-        private void InitializeSpeechRecognition()
+        private void OnSpeechHypothesized(object sender, SpeechHypothesizedEventArgs e)
         {
-            try
-            {
-                var c = new Choices(Cache.Commands.Keys.ToArray());
-                var gb = new GrammarBuilder(c);
-                var g = new Grammar(gb);
-
-                _rec = new SpeechRecognitionEngine();
-                _rec.RecognizeCompleted += OnRecognizeCompleted;
-                _rec.InitialSilenceTimeout = TimeSpan.FromSeconds(3);
-
-                _rec.LoadGrammar(g);
-                _rec.SetInputToDefaultAudioDevice();
-
-                _isEnabled = true;
-            }
-            catch
-            { }
+            _dte.StatusBar.Text = "I'm listening... (" + e.Result.Text + " " + Math.Round(e.Result.Confidence * 100, 0) + "%)";
         }
 
-        private void OnRecognizeCompleted(object sender, RecognizeCompletedEventArgs e)
+        private void OnSpeechRecognized(object sender, RecognizeCompletedEventArgs e)
         {
             _rec.RecognizeAsyncStop();
 
             if (e.Result != null && e.Result.Confidence > _confidence)
             {
-                string text = e.Result.Text;
-
-                if (Cache.Commands.ContainsKey(text) && !TryExecuteCommand(Cache.Commands[text], text))
-                    _dte.StatusBar.Text = text + " is not available in this context";
+                _cache.ExecuteCommand(e.Result.Text);
             }
             else
             {
-                _dte.StatusBar.Text = "I didn't quite get that. Please try again";
+                _dte.StatusBar.Text = "I didn't quite get that. Please try again.";
             }
         }
 
@@ -98,7 +97,7 @@ namespace MadsKristensen.VoiceExtension
         {
             string message = "Do you want to learn how to setup voice recognition in Windows?";
 
-            if (MessageBox.Show(message, "Voice Commands", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            if (MessageBox.Show(message, "Voice Commands for Visual Studio", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
             {
                 using (var process = new System.Diagnostics.Process())
                 {
@@ -106,40 +105,6 @@ namespace MadsKristensen.VoiceExtension
                     process.Start();
                 }
             }
-        }
-
-        private bool TryExecuteCommand(string commandName, string displayName)
-        {
-            try
-            {
-                var command = _dte.Commands.Item(commandName);
-
-                if (command.IsAvailable)
-                {
-                    _dte.ExecuteCommand(command.Name);
-
-                    int index = -1;
-                    var bindings = ((object[])command.Bindings).FirstOrDefault();
-                    string keys = string.Empty;
-
-                    if (bindings != null)
-                    {
-                        keys = bindings.ToString();
-                        index = keys.IndexOf(':') + 2;
-                    }
-
-                    if (index > 1)
-                        _dte.StatusBar.Text = displayName + " (" + keys.Substring(index) + ")";
-                    else
-                        _dte.StatusBar.Text = displayName;
-
-                    return true;
-                }
-            }
-            catch
-            { }
-
-            return false;
         }
     }
 }
